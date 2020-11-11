1f71d01cf12a5e6074d9182f6788fe0ffcfa50c2
redebron20 <redebron20@gmail.com>
2020-11-11 00:13:54 -0500

    commit tree 4b97e27dd1aee9143401350061a4f7258e07dd24

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,25 @@
 # your code goes here
+require 'pry'
+
+
+class Person
+    attr_accessor :name
+    @@people = []
+  
+    def initialize(name)
+      @name = name
+      # self in the initialize method is our new instance
+      # self.class is Person
+      # self.class.all == Person.all
+      self.class.all << self
+    end
+  
+    def self.all
+      @@people
+    end
+  
+    def self.find_by_name(name)
+      self.all.find{|person| person.name == name}
+    end
+  binding.pry
+  end
